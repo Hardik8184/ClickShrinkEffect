6552cc37751d8cdac51397087a96af1705525e83
hdungrani@gotechark.com <p@ssword8184>
2019-07-24 11:24:09 +0530

    master: enhancements

--- shrinkeffect/src/androidTest/java/com/hardik/clickshrinkeffect/ExampleInstrumentedTest.kt
+++ shrinkeffect/src/androidTest/java/com/hardik/clickshrinkeffect/ExampleInstrumentedTest.kt
@@ -1,5 +1,4 @@
 package com.hardik.clickshrinkeffect
-// package com.realpacific.clickshrinkeffect
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
